384e8a27a238f128d97f6075f334ed849b7ac99d
yu-zhen <wlphg010688@gmail.com>
2023-11-05 03:19:23 +0800

    announce the ceremony is paused

--- packages/frontend/src/index.jsx
+++ packages/frontend/src/index.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop'
+import Popup from './components/Popup'
+
 import Routes from './Routes'
 
 const App = () => (
   <BrowserRouter>
+    <Popup
+      open={true}
+      onClose={() => {}}
+      title="Announcement"
+      content="The server is currently paused, we will announce right after it is redeployed."
+      button={<></>}
+    />
     <ScrollToTop />
     <Routes />
   </BrowserRouter>
