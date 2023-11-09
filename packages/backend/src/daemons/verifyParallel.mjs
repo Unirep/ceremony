be137e61013f225c56398f501d647eb7a93d26cb
Chance <jchancehud@gmail.com>
2023-11-08 22:57:26 -0600

    refactor: write verification data to filesystem instead of ipc

--- packages/backend/src/daemons/verifyParallel.mjs
+++ packages/backend/src/daemons/verifyParallel.mjs
@@ -2,6 +2,7 @@ import os from 'os'
 import child_process from 'child_process'
 import url from 'url'
 import path from 'path'
+import fs from 'fs/promises'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
@@ -23,9 +24,17 @@ class ThreadManager {
         new Promise((rs, rj) => {
           const p = child_process.fork(path.join(__dirname, 'verify.mjs'), args)
           let receivedMsg = false
-          p.on('message', (msg) => {
-            receivedMsg = true
-            rs(msg)
+          p.on('message', async (msg) => {
+            try {
+              const { filepath } = msg
+              const data = await fs.readFile(filepath)
+              rs(JSON.parse(data.toString()))
+              receivedMsg = true
+              await fs.rm(filepath)
+            } catch (err) {
+              console.log('unexpected error in verifier message handler')
+              console.log(err)
+            }
           })
           p.on('exit', (code) => {
             if (!receivedMsg && code === 0) {
